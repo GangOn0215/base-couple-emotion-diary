52c19c82c609b255c2c9603632131c4e8cf8f12e
GangOn0215 <ekdldktnekf@gmail.com>
2022-09-20 21:45:57 +0900

    test

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import path from 'path';
 const __dirname = path.resolve();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('build'));
 
