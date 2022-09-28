fcdc1887bfe46b46afb571ec8ffaf3065b77428e
unknown <ekdldktnekf@gmail.com>
2022-09-29 08:51:56 +0900

    test router

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
-import express from 'express';
-import path from 'path';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -8,6 +7,18 @@ const port = process.env.PORT || 3001;
 app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
+
+// router
+const routeTodos = require('./router/todos');
+const routeAccount = require('./router/account');
+
+app.use('/todos', routeTodos);
+app.use('/account', routeAccount);
+
+// 모든 경로 처리 - 리액트 에서 라우터 페이징
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/build/index.html');
+});
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/build/index.html');
@@ -35,11 +46,6 @@ app.get('/todos/list', function (req, res) {
       id: 1,
     },
   ]);
-});
-
-// 모든 경로 처리
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/build/index.html');
 });
 
 app.listen(port, () => {
